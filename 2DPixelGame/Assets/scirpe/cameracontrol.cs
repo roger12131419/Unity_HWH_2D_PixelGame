fdddb3d22c832b415bab8afcb931a85358c81092
roger12131419 <71476649+roger12131419@users.noreply.github.com>
2021-03-26 10:10:59 +0800

    像素遊戲程式攝影機

--- 2DPixelGame/Assets/scirpe/cameracontrol.cs
+++ 2DPixelGame/Assets/scirpe/cameracontrol.cs
@@ -8,4 +8,41 @@ public class cameracontrol : MonoBehaviour
     public Vector2 limitY = new Vector2(-5, 5);
     [Header("左右邊界")]
     public Vector2 limitX = new Vector2(-5, 5);
+    [Header("玩家")]
+    public Transform player;
+    
+    private void Update()
+    {
+       Track();
+    }
+    
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    
+    private void Track()
+    {
+        Vector3 posCam = transform.position; //取得 攝影座標
+        Vector3 posCla = player.position;    //取得 玩家座標
+       
+        posCam = Vector3.Lerp(posCam, posCla, 0.5f * speed * Time.deltaTime);
+        posCam.z = -10;
+        //Mathf.Clamp(要夾住值 ， 最小值 ， 最大值)
+        
+        posCam.x = Mathf.Clamp(posCam.x, limitX.x, limitX.y);
+        posCam.y = Mathf.Clamp(posCam.y, limitY.x, limitY.y);
+        
+        transform.position = posCam;          //更新攝影座標
+
+
+     }
 }
+
+
+
+ 
+
+
+
+
+
