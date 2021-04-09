8beda4b6c8e38d723738811bedda353054e5a63b
roger12131419 <71476649+roger12131419@users.noreply.github.com>
2021-04-09 12:22:36 +0800

    像素遊戲石頭感應區

--- 2DPixelGame/Assets/scirpe/TriggerArea.cs
+++ 2DPixelGame/Assets/scirpe/TriggerArea.cs
@@ -4,4 +4,15 @@ public class TriggerArea : MonoBehaviour
 {
     [Header("要關閉的石頭們")]
     public GameObject[] stones;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "石頭")
+        {
+            Debug.LogError("ok");
+            stones[0].SetActive(false);
+            stones[1].SetActive(false);
+        }
+    }
+
 }
